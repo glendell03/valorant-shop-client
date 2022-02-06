841d77a73c89dc2d11334212bb258d4c88408e1b
Glendell <dellbringino@gmail.com>
2022-02-06 18:12:44 +0800

    feat: working delete

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -6,8 +6,7 @@ import {
   selectorUserWeapons,
 } from "@/features/userWeaponsSlice";
 import Button from "@/components/Button";
-import { deleteWeapons, getUserWeapons } from "@/utils/weapons.routes";
-import axios from "axios";
+import { deleteWeapons } from "@/utils/weapons.routes";
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -24,34 +23,45 @@ const Profile = () => {
   // console.log("state", weaponsData);
 
   const onClickdelete = (uuid) => {
-    // setDataUuid(uuid);
-    deleteWeapons(uuid);
-    dispatch(fetchAllWeapons());
+    setDataUuid(uuid);
+    // deleteWeapons(uuid);
   };
 
-  // useEffect(() => {}, [dataUuid]);
+  useEffect(() => {
+    batch(() => {
+      deleteWeapons(dataUuid);
+      batch(() => {
+        dispatch(resetUserWeaponsState());
+        dispatch(fetchAllWeapons());
+      });
+    });
+  }, [dataUuid]);
 
   return (
     <div className="bg-gray-900 h-screen text-white">
       <div className="container mx-auto">
         <h1 className="text-center font-bold text-5xl py-10">MY WEAPONS</h1>
         <div className="my-10 flex flex-wrap gap-10 justify-center">
-          {weaponsData.map((data) => (
-            <div
-              key={data.id}
-              className="ring ring-white p-10 rounded-xl flex flex-col justify-center items-center gap-10"
-            >
-              <p>{data.displayName}</p>
-              <img
-                src={data.displayIcon}
-                alt={data.displayName}
-                className="h-20"
-              />
-              {/* DELETE BUTTON START HERE */}
+          {weaponsData.length === 0
+            ? "No Data"
+            : weaponsData.map((data) => (
+                <div
+                  key={data.id}
+                  className="ring ring-white p-10 rounded-xl flex flex-col justify-center items-center gap-10"
+                >
+                  <p>{data.displayName}</p>
+                  <img
+                    src={data.displayIcon}
+                    alt={data.displayName}
+                    className="h-20"
+                  />
+                  {/* DELETE BUTTON START HERE */}
 
-              <Button onClick={() => onClickdelete(data.uuid)}>Delete</Button>
-            </div>
-          ))}
+                  <Button onClick={() => onClickdelete(data.uuid)}>
+                    Delete
+                  </Button>
+                </div>
+              ))}
         </div>
       </div>
     </div>
