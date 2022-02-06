63f2b03001fab7f693b766225c5b64cadea28bc9
Andreizabala <Jerichozabala11@gmail.com>
2022-02-06 18:43:38 +0800

    nothing

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector, batch } from "react-redux";
 import {
   fetchAllWeapons,
@@ -7,57 +7,51 @@ import {
 } from "@/features/userWeaponsSlice";
 import Button from "@/components/Button";
 import { deleteWeapons, getUserWeapons } from "@/utils/weapons.routes";
+import axios from "axios";
 
 const Profile = () => {
   const dispatch = useDispatch();
-  const { weaponsData, errorMessage } = useSelector(selectorUserWeapons);
+  const { weaponsData } = useSelector(selectorUserWeapons);
 
   const [dataUuid, setDataUuid] = useState("");
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     batch(() => {
       dispatch(resetUserWeaponsState());
       dispatch(fetchAllWeapons());
     });
   }, []);
+  // console.log("state", weaponsData);
 
   const onClickdelete = (uuid) => {
-    setDataUuid(uuid);
+    // setDataUuid(uuid);
+    deleteWeapons(uuid);
+    dispatch(fetchAllWeapons());
   };
 
-  useEffect(() => {
-    deleteWeapons(dataUuid);
-    batch(() => {
-      dispatch(resetUserWeaponsState());
-      dispatch(fetchAllWeapons());
-    });
-  }, [dataUuid]);
+  // useEffect(() => {}, [dataUuid]);
 
   return (
     <div className="bg-gray-900 h-screen text-white">
       <div className="container mx-auto">
         <h1 className="text-center font-bold text-5xl py-10">MY WEAPONS</h1>
         <div className="my-10 flex flex-wrap gap-10 justify-center">
-          {weaponsData.length === 0
-            ? errorMessage
-            : weaponsData.map((data) => (
-                <div
-                  key={data.id}
-                  className="ring ring-white p-10 rounded-xl flex flex-col justify-center items-center gap-10"
-                >
-                  <p>{data.displayName}</p>
-                  <img
-                    src={data.displayIcon}
-                    alt={data.displayName}
-                    className="h-20"
-                  />
-                  {/* DELETE BUTTON START HERE */}
+          {weaponsData.map((data) => (
+            <div
+              key={data.id}
+              className="ring ring-white p-10 rounded-xl flex flex-col justify-center items-center gap-10"
+            >
+              <p>{data.displayName}</p>
+              <img
+                src={data.displayIcon}
+                alt={data.displayName}
+                className="h-20"
+              />
+              {/* DELETE BUTTON START HERE */}
 
-                  <Button onClick={() => onClickdelete(data.uuid)}>
-                    Delete
-                  </Button>
-                </div>
-              ))}
+              <Button onClick={() => onClickdelete(data.uuid)}>Delete</Button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
