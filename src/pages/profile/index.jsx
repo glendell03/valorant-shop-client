972c9bb8775739ab5cfe553d863ecb8aed79f14b
Glendell <dellbringino@gmail.com>
2022-02-06 18:41:25 +0800

    fx: background

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -34,10 +34,10 @@ const Profile = () => {
   }, [dataUuid]);
 
   return (
-    <div className="bg-gray-900 h-screen text-white">
+    <div className="bg-gray-900 h-full text-white">
       <div className="container mx-auto">
         <h1 className="text-center font-bold text-5xl py-10">MY WEAPONS</h1>
-        <div className="my-10 flex flex-wrap gap-10 justify-center">
+        <div className="py-10 flex flex-wrap gap-10 justify-center h-full">
           {weaponsData.length === 0
             ? errorMessage
             : weaponsData.map((data) => (
@@ -46,11 +46,13 @@ const Profile = () => {
                   className="ring ring-white p-10 rounded-xl flex flex-col justify-center items-center gap-10"
                 >
                   <p>{data.displayName}</p>
+
                   <img
                     src={data.displayIcon}
                     alt={data.displayName}
-                    className="h-20"
+                    className="h-10 w-48"
                   />
+
                   {/* DELETE BUTTON START HERE */}
 
                   <Button onClick={() => onClickdelete(data.uuid)}>
