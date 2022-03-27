c64553cd819181caffae0d37d87f6232f97e0b4a
Glendell <dellbringino@gmail.com>
2022-03-27 20:34:13 +0800

    fa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+ content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   presets: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: { colors: { "vred-primary": "#fa4454", "vred-hover": "#dc3d4b" } },
     screens: {
